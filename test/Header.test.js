b2f52f204f26a229b0709d89e4220c108bdfb69c
Dongmin Ahn <68339352+eamon3481@users.noreply.github.com>
2021-08-21 01:55:27 +0900

    [#4][#5] 날짜 시간 표시되는 header 생성 (#19)

---
+++ test/Header.test.js
@@ -0,0 +1,23 @@
+import { createNowDate, throttle } from '@src/utils/util';
+
+jest.useFakeTimers();
+
+describe('createNowDate', () => {
+  it('현재 날짜 반환', () => {
+    expect(createNowDate(new Date('2019-10-10 12:10:33'))).toBe(
+      '2019년 10월 10일 12시 10분 33초',
+    );
+  });
+
+  it('0.5초에 한번씩 실행', () => {
+    const callback = jest.fn();
+    const th = throttle(callback, 500);
+    th();
+    setTimeout(() => {
+      expect(callback).toBeCalled();
+     
+    }, 500)
+   
+    jest.runAllTimers();
+  });
+});
