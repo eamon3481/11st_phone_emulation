a8073c3d0eadf109611428437f3d4e057cd58a9a
KH-20170630OXUD\Administrator <eamon3481@naver.com>
2021-08-22 22:58:03 +0900

    [#14] feat: 전역 상태 관리를 위한 옵저버와 컴포넌트 클래스 재정비

---
+++ src/lib/Observer.ts
@@ -0,0 +1,54 @@
+
+
+type stateModel = {
+  _state: any;
+  _observers: Set<() => void>;
+};
+
+export type globalStateType = {
+  [key: string]: stateModel;
+};
+
+
+const globalState: globalStateType = {};
+
+const subscribe = (key: string, observer: () => void): void => {
+  globalState[key]._observers.add(observer);
+};
+
+const unsubscribe = (key: string, observer: () => void): void => {
+  globalState[key]._observers.delete(observer);
+};
+
+const _notify = (key: string) => globalState[key]._observers.forEach((observer) => observer());
+
+const initState = <T>({ key, defaultValue }: { key: string; defaultValue: T }): string => {
+  if (key in globalState) throw Error('이미 존재하는 key값 입니다.');
+  globalState[key] = {
+    _state: defaultValue,
+    _observers: new Set(),
+  };
+  return key;
+};
+
+const getState = <T>(key: string): T => {
+  if (!(key in globalState)) throw Error('존재하지 않는 key값 입니다.');
+  return globalState[key]._state;
+};
+
+const setState =
+  <T>(key: string) =>
+  (newState: ((arg: T) => T) | T): void => {
+    if (!(key in globalState)) throw Error('존재하지 않는 key값 입니다.');
+
+    if (newState instanceof Function) {
+      const state = getState<T>(key);
+      globalState[key]._state = newState(state);
+    } else {
+      globalState[key]._state = newState;
+    }
+
+    _notify(key);
+  };
+
+export { subscribe, unsubscribe, initState, getState, setState };
