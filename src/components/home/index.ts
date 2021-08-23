5e583b8ce5431fc0bf2fea4c004522daf929465a
KH-20170630OXUD\Administrator <eamon3481@naver.com>
2021-08-23 14:26:27 +0900

    [#10] Drag & Drop 구현 ( 그리드 )

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,32 +1,90 @@
 import Component from '@src/lib/Component';
 import AppButton from './AppButton';
-import {AppButtonProps} from '@src/type/componentPropsType'
+import {
+  AppButtonProps,
+  createElementType,
+} from '@src/type/componentPropsType';
+import { positionState, PositionStateType } from '@src/store/PositionStore';
+import { getState, setState } from '@src/lib/Observer';
 export default class HomePageBody extends Component {
+  constructor(createElementConfig: createElementType) {
+    super(createElementConfig);
+
+    this.keys = [positionState];
+    this.subscribe();
+  }
+
+  setEvent() {
+    const position = getState<PositionStateType>(positionState);
+    const setPosition = setState<PositionStateType>(positionState);
+    let dragged: { target: 'alert' | 'photo' | 'memo' };
+
+    const handleDragStart = (e: MouseEvent) => {
+      const text = (e.target as HTMLElement).innerText;
+      const textSwitch = (text: string): 'alert' | 'photo' | 'memo' =>
+        text === '알람' ? 'alert' : text === '사진' ? 'photo' : 'memo';
+      const target = textSwitch(text);
+      dragged = { target: textSwitch(text) };
+    };
+
+    const handleDragOver = (e: MouseEvent) => {
+      (e.target as HTMLElement).style.background = 'gray';
+      e.preventDefault();
+    };
+
+    const handleDragLeave = (e: MouseEvent) => {
+      (e.target as HTMLElement).removeAttribute('style');
+    };
+
+    const handleDrop = (e: DragEvent) => {
+      (e.target as HTMLElement).removeAttribute('style');
+      const idx = (e.target as HTMLElement).classList[1];
+      if (isNaN(+idx.slice(4))) {
+      }
+      position[dragged.target] = +idx.slice(4);
+
+      localStorage.setItem('Position', JSON.stringify(position));
+      setPosition(position);
+    };
+
+    this.addEvent('dragstart', '.AppButton', handleDragStart);
+    this.addEvent('dragover', '.home_itemBox', handleDragOver);
+    this.addEvent('dragleave', '.home_itemBox', handleDragLeave);
+    this.addEvent('drop', '.home_itemBox', handleDrop);
+  }
+
   template() {
-    return `
-      ${this.setComponent<AppButton, AppButtonProps>({ tagName: 'div' ,classNames: [ 'home_itemBox']},AppButton, { text: '알람', route: '/alert' }, 0)}
-      ${this.setComponent<AppButton, AppButtonProps>({ tagName: 'div' ,classNames: [ 'home_itemBox']},AppButton, { text: '메모', route: '/memo' }, 1)}
-      ${this.setComponent<AppButton, AppButtonProps>({ tagName: 'div' ,classNames: [ 'home_itemBox']},AppButton, { text: '사진', route: '/photo' }, 2)}
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
-      <div class="home_itemBox"></div>
+    const position = getState<PositionStateType>(positionState);
+    const PositionNumber = 20;
+    const items = new Array(PositionNumber).fill(
+      '<div class="home_itemBox"></div>',
+    );
 
-
-      `;
+    return `${items
+      .map((v, i) => {
+        if (i === position.alert)
+          return this.setComponent<AppButton, AppButtonProps>(
+            { tagName: 'div', classNames: ['home_itemBox', `idx_${i}`] },
+            AppButton,
+            { text: '알람', route: '/alert' },
+            0,
+          );
+        if (i === position.memo)
+          return this.setComponent<AppButton, AppButtonProps>(
+            { tagName: 'div', classNames: ['home_itemBox', `idx_${i}`] },
+            AppButton,
+            { text: '메모', route: '/memo' },
+            1,
+          );
+        if (i === position.photo)
+          return this.setComponent<AppButton, AppButtonProps>(
+            { tagName: 'div', classNames: ['home_itemBox', `idx_${i}`] },
+            AppButton,
+            { text: '사진', route: '/photo' },
+            2,
+          );
+        return `<div class="home_itemBox idx_${i}"></div>`;
+      })
+      .join('')}`;
   }
 }
---
+++ src/store/PositionStore.ts
@@ -0,0 +1,22 @@
+import { initState } from '@src/lib/Observer';
+
+export type PositionStateType = {
+  alert: number;
+  photo: number;
+  memo: number;
+};
+const defaultPositionValue = {
+  alert: 0,
+  photo: 1,
+  memo: 2,
+};
+
+const storagePosition = localStorage.getItem('Position');
+if (!storagePosition)
+  localStorage.setItem('Position', JSON.stringify(defaultPositionValue));
+export const positionState = initState<PositionStateType>({
+  key: 'positionState',
+  defaultValue: storagePosition
+    ? JSON.parse(storagePosition)
+    : defaultPositionValue,
+});
